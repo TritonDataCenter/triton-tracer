3ca824836c7a4c958fb9dd7e63f0ecb97a4d0e5e
Josh Wilsdon <jwilsdon@joyent.com>
2016-10-18 23:33:11 -0700

    wip

--- index.js
+++ index.js
@@ -16,31 +16,18 @@ var TritonTracerRestifyServer = require('./lib/restify-server.js');
 
 var alreadyRun = false;
 
-function init(options, callback) {
+function init(options) {
     // This function is only ever intended to be called once per program.
     // TODO: add test for this.
     assert.equal(alreadyRun, false, 'init() must only be called once');
     alreadyRun = true;
 
-    var session = cls.createNamespace(TritonTracerConstants.CLS_NAMESPACE);
+    process.TritonCLS = cls.createNamespace(TritonTracerConstants.CLS_NAMESPACE);
     var Tracer = require('opentracing');
 
+    process.TritonCLS.run(function () {});
     opentracing.initGlobalTracer(new TritonTracerOpenTracer(options));
-
-    session.run(function _callTracerInitCallback() {
-        // XXX
-        //
-        // This seems kinda unfortunate, but I've not yet thought of a better
-        // way for functions elsewhere to find the session.
-        //
-        process.tritonTracer = {
-            getSession: session.bind(function getSession() {
-                return (cls.getNamespace(TritonTracerConstants.CLS_NAMESPACE));
-            })
-        }
-        session.set('TritonTracer', opentracing.globalTracer());
-        callback(session);
-    });
+    process.TritonCLS.set('TritonTracer', opentracing.globalTracer());
 }
 
 module.exports = {
