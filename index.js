af38788217b836da71b6691a7ebc295447b800ee
Josh Wilsdon <jwilsdon@joyent.com>
2016-10-21 16:41:53 -0700

    cleanup for CLS

--- index.js
+++ index.js
@@ -7,36 +7,30 @@
 //
 
 var assert = require('assert-plus');
-var cls = require('continuation-local-storage');
-var opentracing = require('opentracing');
+
 var TritonTracerConstants = require('./lib/ot-constants.js');
-var TritonTracerOpenTracer = require('./lib/ot-tracer-imp.js');
 var TritonTracerRestifyClient = require('./lib/restify-client.js');
 var TritonTracerRestifyServer = require('./lib/restify-server.js');
 
-var alreadyRun = false;
+var _global = require('./global');
 
-function init(options, callback) {
-    // This function is only ever intended to be called once per program.
-    // TODO: add test for this.
-    assert.equal(alreadyRun, false, 'init() must only be called once');
-    alreadyRun = true;
+function init(options) {
+    _global.init(options);
+}
 
-    process.TritonCLS = cls.createNamespace(TritonTracerConstants.CLS_NAMESPACE);
+function getTracer() {
+    return (_global.tracer());
+}
 
-    // initialize opentracing using the TritonTracer implementation
-    opentracing.initGlobalTracer(new TritonTracerOpenTracer(options));
-
-    process.TritonCLS.run(function _callInitCallback() {
-        process.TritonCLS.set('TritonTracer', opentracing.globalTracer());
-        callback();
-    });
+function getCLS() {
+    return (_global.getCLS());
 }
 
 module.exports = {
+    cls: getCLS,
     consts: TritonTracerConstants,
     init: init,
-    opentracer: TritonTracerOpenTracer,
     restifyClient: TritonTracerRestifyClient,
-    restifyServer: TritonTracerRestifyServer
+    restifyServer: TritonTracerRestifyServer,
+    tracer: getTracer
 };
